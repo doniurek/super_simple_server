3235a0fc061f06c5f83302fd81a44c80131246ba
doniurek <aldonaglebicka@gmail.com>
2021-02-24 20:42:13 +0100

    add request body to logs + refactor

--- sss_app.js
+++ sss_app.js
@@ -9,22 +9,23 @@ console.log("Listening on port " + PORT);
 console.log("-------------------------");
 
 function handleRequest(request, response) {
-  newRequestLog(request);
-
-  const data = {recipe: "butter the bread"};
-  const json = JSON.stringify(data);
-
-  response.writeHead(200, {'Content-Type': 'application/json'});
-  response.end(json);
+  let body = "";
+  request.on("data", chunk => { body += chunk; });
+  request.on("end", () => {
+    newRequestLog(request, body);
+    respond(response);
+  });
 };
 
-function newRequestLog(request) {
+function newRequestLog(request, body) {
   console.log(
     "New request: "         +
     protocol(request) + " " +
     request.method    + " " +
     request.headers.host    +
-    request.url
+    request.url      + "\n" +
+    "Reqest body: "         +
+    JSON.stringify(JSON.parse(body), null, 2)
   );
 };
 
@@ -37,4 +38,12 @@ function protocol(request) {
     default:
       return "UNSUPPORTED PROTOCOL [" + request.protocol + "]";
   }
+};
+
+function respond(response) {
+  const data = {recipe: "butter the bread"};
+  const json = JSON.stringify(data);
+
+  response.writeHead(200, {'Content-Type': 'application/json'});
+  response.end(json);
 };
