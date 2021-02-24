a18bb5ff366848721cc1e3cd23d2e3e1fa30ce06
doniurek <aldonaglebicka@gmail.com>
2021-02-24 18:41:13 +0100

    extract newRequestLog function

--- sss_app.js
+++ sss_app.js
@@ -2,18 +2,27 @@ const http = require('http');
 
 const PORT = 2137;
 
-function handleRequest(req, res) {
-  console.log("New request: " + req.method + " " + req.headers.host + req.url);
-
-  const data = {recipe: "butter the bread"};
-  const json = JSON.stringify(data);
-
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(json);
-};
-
 let app = http.createServer(handleRequest);
 console.log("Server is up!");
 app.listen(PORT);
 console.log("Listening on port " + PORT);
 console.log("-------------------------");
+
+function handleRequest(request, response) {
+  newRequestLog(request);
+
+  const data = {recipe: "butter the bread"};
+  const json = JSON.stringify(data);
+
+  response.writeHead(200, {'Content-Type': 'application/json'});
+  response.end(json);
+};
+
+function newRequestLog(request) {
+  console.log(
+    "New request: "  +
+    request.method + " " +
+    request.headers.host +
+    request.url
+  );
+};
